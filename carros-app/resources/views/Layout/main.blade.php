8055b3dc259ca25ab6d41d35093133edc821a75e
IvanCarlos1999 <ivancarlosgoncalves1999@gmail.com>
2023-01-10 12:36:18 +0000

    Formulario e tamplate

---
+++ carros-app/resources/views/Layout/main.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE HTML>
+<!--
+	Phantom by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+	<head>
+		<title>@yield ('title')</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="/css/main.css" />
+        <noscript><link rel="stylesheet" href="/css/noscript.css" /></noscript>
+	</head>
+	<body class="is-preload">
+		@yield ('content')
+
+	</body>
+
+    <footer>Ivan Gonçalves @copy 2023</footer>
+</html>
